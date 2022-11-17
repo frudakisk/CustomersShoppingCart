a651f2edd9bca2531f17cc14034ff2db01f6717e
kfruda <frudakisk@gmail.com>
2022-11-17 14:44:42 -0500

    Created User class and Item class
    bare bones classes for backend usage

---
+++ CustomersShoppingCart/src/main/java/customersshoppingcart/Items.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package customersshoppingcart;
+
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author fruda
+ */
+public class Items {
+    
+    public Items(int itemId, String name, String description, double price, int quantity) {
+        this.itemId = itemId;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.quantity = quantity;
+        //this.image = image;
+    }
+    
+    public void updatePrice(double newPrice) {
+        this.price = newPrice;
+    }
+    
+    public void updateQuantity(int newQuantity) {
+        this.quantity = newQuantity;
+    }
+    
+    public void updateQuantitySold(int newSold) {
+        this.quantitySold = newSold;
+    }
+    
+    public void updateName(String newName) {
+        this.name = newName;
+    }
+    
+    public void updateDescription(String newDescription) {
+        this.description = newDescription;
+    }
+    
+    public void updateImage(ImageIcon newImage) {
+        this.image = newImage;
+    }
+    
+    
+    //Attributes
+    public int itemId;
+    public String name;
+    public String description;
+    public double price;
+    public int quantity;
+    public int quantitySold;
+    public ImageIcon image;
+}
